8d71b9752481a062ccabb02ff763b8b54fae34ed
kibalias <74294078+kibalias@users.noreply.github.com>
2022-06-12 20:25:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/bacterial_blight/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.bacterial_blight;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import com.example.bacterial_blight.ml.Model;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+
+import org.checkerframework.common.subtyping.qual.Bottom;
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class MainActivity extends AppCompatActivity {
+    Button select, scan;
+    ImageView placeholder;
+    TextView result, confidence, link;
+    RadioGroup radioGroup;
+    RadioButton chosenModel;
+    Bitmap image = null;
+    int imageSize =  224;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        select = findViewById(R.id.upload_button);
+        scan = findViewById(R.id.scan_button);
+        result = findViewById(R.id.result);
+        placeholder = findViewById(R.id.image_placeholder);
+        radioGroup = findViewById(R.id.radioGroup);
+
+        //Select Image Button
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+
+        //Scan Button
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Resizes the image for classification
+                image = Bitmap.createScaledBitmap(image, imageSize, imageSize, false);
+
+                int model = radioGroup.getCheckedRadioButtonId();
+                chosenModel = findViewById(model);
+
+                switch(model){
+                    case R.id.vggRadioBtn:
+                        result.setText("Feature coming soon.");
+                        break;
+                    case R.id.resNetRadioBtn:
+                        resNet(image);
+                        break;
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+            if(resultCode == RESULT_OK && data != null){
+                //Gets image from the gallery and displays to placeholder
+                Uri selectedImage = data.getData();
+
+                try{
+                    image = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+                placeholder.setImageBitmap(image);
+            }
+        }
+
+    public void resNet(Bitmap image){
+        try {
+            Model model = Model.newInstance(getApplicationContext());
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int[] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+            int pixel = 0;
+            //iterate over each pixels and extract RGB values. Values will be added individually to the byte buffer
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; //RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f/1));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f/1));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f/1));
+                }
+            }
+
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            Model.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            float[] confidences = outputFeature0.getFloatArray();
+            //find the index of class with big confidence
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for (int i = 0; i < confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+
+            String[] classes = {"CBB", "Healthy"};
+            //Display the class of the prediction
+            result.setText(classes[maxPos]);
+
+            resultPop(classes[maxPos]);
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+    }
+
+    public void resultPop(String result){
+
+        BottomSheetDialog bottom_sheet = new BottomSheetDialog(MainActivity.this,R.style.BottomSheetTheme);
+        View sheetView = null;
+        if(result == "CBB"){
+            //display the bottom sheet result
+
+            sheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_pop_result,
+                    findViewById(R.id.popUpContainer));
+        } else if(result == "Healthy"){
+            sheetView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_pop_healthy_result,
+                    findViewById(R.id.popUpContainer));
+        }
+        bottom_sheet.setContentView(sheetView);
+        bottom_sheet.show();
+    }
+}
